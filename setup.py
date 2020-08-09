db2906bc3772271f89d4938bc5fb3c7c7181b0d2
unknown <manu.somasagar.kamalakar@itwm.fraunhofer.de>
2020-08-09 09:58:52 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' applied data science on COVID-19 data',
+    author='Manu Somasagar K[C[C[C[C[C[C[C[C[C[C[3~[3~[3~[D[D[D[D[D[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cu Somasagar Kamalakar',
+    license='',
+)
